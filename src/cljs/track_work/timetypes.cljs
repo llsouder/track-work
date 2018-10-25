74133aa44539e5d92f7759a39e0c0f70256da30e
Lonnie Souder <lonnie@souderfamily.net>
2018-10-25 17:22:29 -0400

    wiring up button using re-frame dispatch/subscribe.

--- src/cljs/track_work/timetypes.cljs
+++ src/cljs/track_work/timetypes.cljs
@@ -1,12 +1,48 @@
 (ns track-work.timetypes
   (:require [ajax.core :as ajax]
+            [re-frame.core :as rf]
+            [clojure.string :as string]
             [reagent.core :as r]))
 
+(defn handle-change-name
+  [db [_ name]]
+  (assoc db :name name))
+
+(rf/reg-event-db :change-name handle-change-name)
+
+(defn error []
+  (js/alert "error"))
+
+(defn add-type [name description]
+  (js/alert (str "adding " name)))
+
 (defn type-form []
-  [:form {:action "/add_type" :method "POST"}
-   [:input {:id "name" :name "name" :type "text"}]
-   [:input {:id "description" :name "description" :type "text"}]
-   [:input {:id "submit" :type "submit"}]])
+  [:div
+   [:form
+    "Time Type:" [:br]
+    [:input {:id "name"
+             :name "name"
+             :type "text"
+             :on-change #(rf/dispatch
+                          [:change-name (-> % .-target .-value)])}][:br]
+    "Description:" [:br]
+    [:input {:id "description"
+             :name "description"
+             :type "text"}][:br]
+
+    [:input {:value "submit"
+             :type "button"
+             :on-click (fn [event] (rf/dispatch [:add-type-click]))}][:br]]])
+
+(defn handle-add-type
+  [{:keys [name description] :as db} _]
+  (println "read a book")
+  (if (string/blank? name )
+    (error)
+    (add-type name description))
+  db)
+
+(rf/reg-event-db :add-type-click handle-add-type)
 
 (def timetypes (r/atom nil))
 

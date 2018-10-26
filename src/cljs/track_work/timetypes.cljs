f2f450de2a48a28e83b6dec88b97f34e578f157a
Lonnie Souder <lonnie@souderfamily.net>
2018-10-26 09:15:00 -0400

    Wire up the form using dispatch/sub.

--- src/cljs/track_work/timetypes.cljs
+++ src/cljs/track_work/timetypes.cljs
@@ -4,62 +4,76 @@
             [clojure.string :as string]
             [reagent.core :as r]))
 
-(defn handle-change-name
-  [db [_ name]]
-  (assoc db :name name))
+(def timetypes (r/atom nil))
 
-(rf/reg-event-db :change-name handle-change-name)
+(defn handle-change-timetype
+  [db [_ timetype]]
+  (assoc db :timetype timetype))
+
+(rf/reg-event-db :change-timetype handle-change-timetype)
+
+(defn handle-change-desc
+  [db [_ description]]
+  (assoc db :description description))
+
+(rf/reg-event-db :change-desc handle-change-desc)
 
 (defn error []
   (js/alert "error"))
 
-(defn add-type [name description]
-  (js/alert (str "adding " name)))
+(defn add-type [timetype description]
+  (js/alert (str "adding " timetype description)))
 
 (defn type-form []
   [:div
    [:form
     "Time Type:" [:br]
-    [:input {:id "name"
-             :name "name"
+    [:input {:id "timetype"
+             :name "timetype"
              :type "text"
              :on-change #(rf/dispatch
-                          [:change-name (-> % .-target .-value)])}][:br]
+                          [:change-timetype (-> % .-target .-value)])}][:br]
     "Description:" [:br]
     [:input {:id "description"
              :name "description"
-             :type "text"}][:br]
+             :type "text"
+             :on-change #(rf/dispatch
+                 [:change-desc (-> % .-target .-value)])}][:br]
 
     [:input {:value "submit"
              :type "button"
              :on-click (fn [event] (rf/dispatch [:add-type-click]))}][:br]]])
 
+(defn set-types! [response]
+  (reset! timetypes response))
+
+
+(defn get-types []
+  (println "getting the types")
+  (ajax/GET "/get_types" {:handler set-types!
+                          :response-format :json
+                          :keywords? true}))
+
 (defn handle-add-type
-  [{:keys [name description] :as db} _]
-  (println "read a book")
-  (if (string/blank? name )
+  [{:keys [timetype description] :as db} _]
+  (if (string/blank? timetype )
     (error)
-    (add-type name description))
+    (do
+      (println (str "POST " timetype " " description))
+      (ajax/POST "add_type"
+                {:params {:time_type timetype
+                          :description description}
+                 :handler get-types})))
   db)
 
 (rf/reg-event-db :add-type-click handle-add-type)
-
-(def timetypes (r/atom nil))
 
 (defn make-row
   [timetype]
    [:tr {:key (:id timetype)}
     [:td (:id timetype)]
-    [:td (:name timetype)]
+    [:td (:time_type timetype)]
     [:td (:description timetype)]])
-
-(defn set-types! [response]
-  (reset! timetypes response))
-
-(defn get-types []
-  (ajax/GET "/get_types" {:handler set-types!
-                          :response-format :json
-                          :keywords? true}))
 
 (defn list-types []
   (get-types)
@@ -67,7 +81,7 @@
    [:table {:border "1"}
     [:tbody
      [:tr
-      [:th "Id"] [:th "Name"] [:th "Description"]]
+      [:th "Id"] [:th "Time Type"] [:th "Description"]]
       (map make-row @timetypes)]]))
 
 (defn page []

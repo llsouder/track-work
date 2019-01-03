edf417310c5a0a0063c52605217ea4cf46d94f26
Lonnie L. Souder <lonnie@souderfamily.net>
2019-01-02 22:35:57 -0500

    checkbox toggle working but page flickers too much.

--- src/cljs/track_work/bubble.cljs
+++ src/cljs/track_work/bubble.cljs
@@ -7,18 +7,18 @@
     (ajax/GET (str "/api/get_bubbles/" id
                    {:handler #(rf/dispatch :set-bubble-map [[id %1]])}))))
 
-(defn add-bubble [id]
+(defn add-bubble [id get-task-fn]
   (ajax/POST "/api/add_bubble"
              {:params {:task_id id}
               :format :json
-              :handler #((get-bubbles id))
+              :handler #(get-task-fn id)
               :error #(println (str "error:" id))}))
 
-(defn remove-bubble [id]
+(defn remove-bubble [id get-task-fn]
   (ajax/POST "/api/remove_bubble"
              {:params {:task_id id}
               :format :json
-              :handler #((get-bubbles id))
+              :handler #(get-task-fn id)
               :error #(println (str "error:" id))}))
 
 (defn check-bubble
@@ -27,18 +27,18 @@
   (-> event .-target .-checked))
 
 (defn toggle-bubble
-  [event task_id]
+  [event task_id get-task-fn]
   (if (check-bubble event)
-    (add-bubble task_id)
-    (remove-bubble task_id)))
+    (add-bubble task_id get-task-fn)
+    (remove-bubble task_id get-task-fn)))
 
-(defn make-bubbles [task_id checks]
+(defn make-bubbles [task_id checks get-tasks-fn]
   (map
    (fn [n check]
      [:input {:id (str task_id "-" n)
               :type "checkbox"
               :checked (not-empty check)
-              :on-click #(toggle-bubble %1 task_id)}])
+              :on-click #(toggle-bubble %1 task_id get-tasks-fn)}])
    (range (count checks))
    checks))
 
--- src/cljs/track_work/task.cljs
+++ src/cljs/track_work/task.cljs
@@ -29,9 +29,9 @@
 (defn get-tasks [id]
   (if id
     (ajax/GET (str "/api/get_tasks/" id)
-      {:handler #(rf/dispatch [:update-task %1])
-       :response-format :json
-       :keywords? true})))
+              {:handler #(rf/dispatch [:update-task %1])
+               :response-format :json
+               :keywords? true})))
 
 (defn handle-add-task
   "Add a new task."
@@ -39,14 +39,14 @@
   (if (string/blank? task)
     (rf/dispatch [:set-task_error "Task cannot be blank."])
     (ajax/POST "/api/add_task"
-      {:params {:proj_id  proj_id
-                :task_desc task}
-       :format :json
-       :error #(rf/dispatch [:set-task_error
-                             (str "Task not added, proj_id:" proj_id)])
-       :handler #(do (rf/dispatch [:change-task ""])
-                     (println "posting task:")
-                     (get-tasks proj_id))}))
+               {:params {:proj_id  proj_id
+                         :task_desc task}
+                :format :json
+                :error #(rf/dispatch [:set-task_error
+                                      (str "Task not added, proj_id:" proj_id)])
+                :handler #(do (rf/dispatch [:change-task ""])
+                              (println "posting task:")
+                              (get-tasks proj_id))}))
   db)
 
 (rf/reg-event-db :add-task-click handle-add-task)
@@ -60,7 +60,7 @@
 
 (defn just-the-tasks
   [tasks]
-    (distinct (map #(dissoc %1 :bubble) tasks)))
+  (distinct (map #(dissoc %1 :bubble) tasks)))
 
 (defn make-row
   [task tasks]
@@ -68,7 +68,7 @@
         bubbles (extract-from id tasks)]
     [:tr {:key id}
      [:td task_desc]
-     [:td (bubble/make-bubbles id bubbles)]]))
+     [:td (bubble/make-bubbles id bubbles get-tasks)]]))
 
 (defn list-task []
   (let [proj_id (rf/subscribe [:proj_id])
@@ -77,27 +77,13 @@
     (fn []
       (get-tasks @proj_id)
       (let [task-list (just-the-tasks @tasks)]
-      [:div
-       [:h4 (if (nil? @proj_desc)
-              "no project selected"
-              @proj_desc)]
-       [:table.table 
-        [:tbody
-         (map #(make-row %1 @tasks) task-list)]]]))))
-
-(defn dropdown-task []
-  (let [proj_id (rf/subscribe [:proj_id])
-        proj_desc (rf/subscribe [:proj_desc])
-        tasks (rf/subscribe [:tasks])]
-    (fn []
-      (get-tasks @proj_id)
-      [:div
-       [:h4 (if (nil? @proj_desc)
-              "no project selected"
-              @proj_desc)]
-       [:table.table 
-        [:tbody
-         (map make-row @tasks)]]])))
+        [:div
+         [:h4 (if (nil? @proj_desc)
+                "No project selected."
+                @proj_desc)]
+         [:table.table 
+          [:tbody
+           (map #(make-row %1 @tasks) task-list)]]]))))
 
 (defn component []
   [:div.task-component
